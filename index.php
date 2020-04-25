204669230d652edf4ae46a0c85288f8bdd5a9b55
Kevin Glenanda Aditiya <62018139+kevachiko23@users.noreply.github.com>
2020-04-25 22:33:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+echo "<center>";
+echo "<h1>Data Pemantauan Covid 19 Wilayah DKI JAKARTA</h1>";
+echo "<h2> Per Hari Ini ".date('l, d-M-Y / H:i:s a');
+echo "</tr>";
+echo "<table border =4 width =600 align = center>";
+
+echo "</tr>";
+echo "<td>Positif</td>";
+echo "<td>Dirawat</td>";
+echo "<td>Sembuh</td>";
+echo "<td>Meninggal</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>8.607</td>";
+echo "<td>6.845</td>";
+echo "<td>1042</td>";
+echo "<td>720</td>";
+echo "</tr>";
+echo "<tr>";
+
+echo "</table>";
+echo "</center>";
+?>
